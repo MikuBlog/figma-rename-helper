6fc378de0ca897d1a96ac1ee8ab6e375a2f88155
旋仔 <zixuan.wen@shopcider.com>
2024-05-13 10:45:44 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-05-11 18:08:49
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-11 19:17:13
+ * @LastEditTime: 2024-05-13 10:45:06
  * @FilePath: /figma-plugin-vue3-template/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
