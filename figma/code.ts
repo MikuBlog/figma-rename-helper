65ed8ed0afa5ce722167ad3aeee934c84305722a
旋仔 <zixuan.wen@shopcider.com>
2024-05-15 11:12:34 +0800

    fix: 点击确认关闭插件

--- figma/code.ts
+++ figma/code.ts
@@ -2,7 +2,7 @@
  * @Author: 旋仔 zixuan.wen@shopcider.com
  * @Date: 2024-04-30 10:51:17
  * @LastEditors: 旋仔 zixuan.wen@shopcider.com
- * @LastEditTime: 2024-05-14 16:01:28
+ * @LastEditTime: 2024-05-15 11:11:16
  * @FilePath: /cider-figma-plugin/code.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -52,7 +52,7 @@ const handleConfirmRename = (newList: Array<NewNodeType>) => {
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'confirm') {
     handleConfirmRename(msg.value)
-    figma.showUI(__html__, { visible: false })
+    figma.closePlugin()
   }
 }
 
